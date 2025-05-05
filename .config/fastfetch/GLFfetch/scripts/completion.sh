d4609b09d91db9b05525ce46c0ec9785a597b01b
Teilenh <125691903+Teilenh@users.noreply.github.com>
2025-05-05 07:22:52 +0200

    Delete .config/fastfetch/GLFfetch directory

--- .config/fastfetch/GLFfetch/scripts/completion.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/zsh
-source ~/.config/fastfetch/GLFfetch/scripts/vars.sh
-
-if [[ $percentage -lt 100 ]]
-then
-  ProgressBar ${install_time} ${end_challenge}
-else [[ $percentage -ge 100 ]]
-  echo "GLF Challenge completed"
-fi
