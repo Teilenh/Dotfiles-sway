c2f270c452df796eb3b3388b94588ccb5e87879a
Teilenh <125691903+Teilenh@users.noreply.github.com>
2025-08-13 00:31:49 +0200

    Update installScript.sh

--- installScript.sh
+++ installScript.sh
@@ -10,7 +10,7 @@ USER="${SUDO_USER:-$(whoami)}"
 USER_UID="$(id -u ${USER})"
 
 echo "###############################################################"
-echo "🚀 SCRIPT POST-INSTALLATION ALPINE LINUX GAMING"
+echo "SCRIPT POST-INSTALLATION ALPINE LINUX GAMING"
 echo "###############################################################"
 
 
